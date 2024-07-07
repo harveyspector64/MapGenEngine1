40a9025d076d124c5360136e7eb5af8a0cc98299
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 19:19:54 -0700

    Update Decoration.js

--- scripts/Decoration.js
+++ scripts/Decoration.js
@@ -1,9 +1,19 @@
+// File: scripts/Decoration.js
+
 class Decoration {
     constructor(tileMap) {
         this.tileMap = tileMap;
     }
 
     placeDecorations() {
-        // Place trees, bushes, hills, barns, and silos based on rules
+        for (let y = 0; y < this.tileMap.map.length; y++) {
+            for (let x = 0; x < this.tileMap.map[y].length; x++) {
+                if (this.tileMap.map[y][x] === 'grass' && Math.random() < 0.1) {
+                    this.tileMap.map[y][x] = 'tree';
+                } else if (this.tileMap.map[y][x] === 'grass' && Math.random() < 0.05) {
+                    this.tileMap.map[y][x] = 'bush';
+                }
+            }
+        }
     }
 }
