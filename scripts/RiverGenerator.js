e47df76ab4e10acb9020f318fd1cba9500ff091d
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 19:07:21 -0700

    Create RiverGenerator.js

---
+++ scripts/RiverGenerator.js
@@ -0,0 +1,10 @@
+class RiverGenerator {
+    constructor(tileMap, aStar) {
+        this.tileMap = tileMap;
+        this.aStar = aStar;
+    }
+
+    generateRivers() {
+        // Use A* to generate rivers with realistic flow and curvature
+    }
+}
