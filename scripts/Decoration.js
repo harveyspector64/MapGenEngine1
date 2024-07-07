a94f4151bae53763806ee935fa310b06da681869
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 22:10:54 -0700

    Update Decoration.js

--- scripts/Decoration.js
+++ scripts/Decoration.js
@@ -5,52 +5,25 @@
         }
 
         placeDecorations() {
-            const treeClusters = this.createTreeClusters();
-            const bushClusters = this.createBushClusters();
-
-            for (let cluster of treeClusters) {
-                for (let { x, y } of cluster) {
-                    if (this.isWithinBounds(x, y) && this.tileMap.map[y][x] === 'grass') {
-                        this.tileMap.map[y][x] = 'tree';
-                    }
-                }
-            }
-
-            for (let cluster of bushClusters) {
-                for (let { x, y } of cluster) {
-                    if (this.isWithinBounds(x, y) && this.tileMap.map[y][x] === 'grass') {
-                        this.tileMap.map[y][x] = 'bush';
-                    }
-                }
-            }
+            this.placeClusters('tree', 10, 5);
+            this.placeClusters('bush', 8, 3);
         }
 
-        createTreeClusters() {
-            return this.createClusters('tree', 10, 5, 5);
-        }
-
-        createBushClusters() {
-            return this.createClusters('bush', 10, 3, 3);
-        }
-
-        createClusters(type, clusterCount, clusterWidth, clusterHeight) {
-            const clusters = [];
+        placeClusters(tileType, clusterCount, clusterSize) {
+            const map = this.tileMap.map;
 
             for (let i = 0; i < clusterCount; i++) {
-                const startX = Math.floor(Math.random() * (this.tileMap.map[0].length - clusterWidth));
-                const startY = Math.floor(Math.random() * (this.tileMap.map.length - clusterHeight));
-                const cluster = [];
+                const startX = Math.floor(Math.random() * (map[0].length - clusterSize));
+                const startY = Math.floor(Math.random() * (map.length - clusterSize));
 
-                for (let x = startX; x < startX + clusterWidth; x++) {
-                    for (let y = startY; y < startY + clusterHeight; y++) {
-                        cluster.push({ x, y });
+                for (let x = startX; x < startX + clusterSize; x++) {
+                    for (let y = startY; y < startY + clusterSize; y++) {
+                        if (this.isWithinBounds(x, y)) {
+                            map[y][x] = tileType;
+                        }
                     }
                 }
-
-                clusters.push(cluster);
             }
-
-            return clusters;
         }
 
         isWithinBounds(x, y) {
