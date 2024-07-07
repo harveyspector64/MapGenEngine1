99c547edaa0a8b5d2ce34efd5b379b88d2464120
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 21:07:47 -0700

    Update RiverGenerator.js

--- scripts/RiverGenerator.js
+++ scripts/RiverGenerator.js
@@ -1,63 +1,69 @@
 // File: scripts/RiverGenerator.js
 
-class RiverGenerator {
-    constructor(tileMap, aStar) {
-        this.tileMap = tileMap;
-        this.aStar = aStar;
-    }
-
-    generateRivers() {
-        const rows = this.tileMap.map.length;
-        const cols = this.tileMap.map[0].length;
-        const start = { x: 0, y: Math.floor(rows / 2) };
-        const end = { x: cols - 1, y: Math.floor(rows / 2) };
-        const path = this.aStar.findPath(start, end);
-
-        for (let node of path) {
-            this.tileMap.map[node.y][node.x] = 'water';
+(function(global) {
+    class RiverGenerator {
+        constructor(tileMap, aStar) {
+            this.tileMap = tileMap;
+            this.aStar = aStar;
         }
 
-        // Add meandering effect
-        this.addMeanders(path);
+        generateRivers() {
+            const rows = this.tileMap.map.length;
+            const cols = this.tileMap.map[0].length;
+            const start = { x: 0, y: Math.floor(rows / 2) };
+            const end = { x: cols - 1, y: Math.floor(rows / 2) };
+            const path = this.aStar.findPath(start, end);
 
-        // Add lakes and ponds
-        this.addLakesAndPonds();
-    }
-
-    addMeanders(path) {
-        for (let i = 1; i < path.length - 1; i++) {
-            const node = path[i];
-            if (Math.random() < 0.3) {
-                const direction = Math.random() > 0.5 ? 1 : -1;
-                if (this.isWithinBounds(node.x + direction, node.y)) {
-                    this.tileMap.map[node.y][node.x + direction] = 'water';
-                }
-                if (this.isWithinBounds(node.x, node.y + direction)) {
-                    this.tileMap.map[node.y + direction][node.x] = 'water';
-                }
+            for (let node of path) {
+                this.tileMap.map[node.y][node.x] = 'water';
             }
+
+            // Add meandering effect
+            this.addMeanders(path);
+
+            // Add lakes and ponds
+            this.addLakesAndPonds();
         }
-    }
 
-    addLakesAndPonds() {
-        const lakeCount = 3;
-        const lakeSize = 4;
-
-        for (let i = 0; i < lakeCount; i++) {
-            const startX = Math.floor(Math.random() * this.tileMap.map[0].length);
-            const startY = Math.floor(Math.random() * this.tileMap.map.length);
-
-            for (let x = startX; x < startX + lakeSize; x++) {
-                for (let y = startY; y < startY + lakeSize; y++) {
-                    if (this.isWithinBounds(x, y)) {
-                        this.tileMap.map[y][x] = 'water';
+        addMeanders(path) {
+            for (let i = 1; i < path.length - 1; i++) {
+                const node = path[i];
+                if (Math.random() < 0.3) {
+                    const direction = Math.random() > 0.5 ? 1 : -1;
+                    if (this.isWithinBounds(node.x + direction, node.y)) {
+                        this.tileMap.map[node.y][node.x + direction] = 'water';
+                    }
+                    if (this.isWithinBounds(node.x, node.y + direction)) {
+                        this.tileMap.map[node.y + direction][node.x] = 'water';
                     }
                 }
             }
         }
+
+        addLakesAndPonds() {
+            const lakeCount = 3;
+            const lakeSize = 4;
+
+            for (let i = 0; i < lakeCount; i++) {
+                const startX = Math.floor(Math.random() * this.tileMap.map[0].length);
+                const startY = Math.floor(Math.random() * this.tileMap.map.length);
+
+                for (let x = startX; x < startX + lakeSize; x++) {
+                    for (let y = startY; y < startY + lakeSize; y++) {
+                        if (this.isWithinBounds(x, y)) {
+                            this.tileMap.map[y][x] = 'water';
+                        }
+                    }
+                }
+            }
+        }
+
+        isWithinBounds(x, y) {
+            return x >= 0 && x < this.tileMap.map[0].length && y >= 0 && y < this.tileMap.map.length;
+        }
     }
 
-    isWithinBounds(x, y) {
-        return x >= 0 && x < this.tileMap.map[0].length && y >= 0 && y < this.tileMap.map.length;
-    }
-}
+    // Expose RiverGenerator to the global object
+    global.RiverGenerator = RiverGenerator;
+
+})(window);
