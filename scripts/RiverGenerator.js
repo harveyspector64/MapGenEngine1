22c418cc1b1a386bd60b95cfc8d0835f2366033a
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 19:19:44 -0700

    Update RiverGenerator.js

--- scripts/RiverGenerator.js
+++ scripts/RiverGenerator.js
@@ -1,3 +1,5 @@
+// File: scripts/RiverGenerator.js
+
 class RiverGenerator {
     constructor(tileMap, aStar) {
         this.tileMap = tileMap;
@@ -5,6 +7,12 @@ class RiverGenerator {
     }
 
     generateRivers() {
-        // Use A* to generate rivers with realistic flow and curvature
+        const start = { x: 0, y: Math.floor(this.tileMap.map.length / 2) };
+        const end = { x: this.tileMap.map[0].length - 1, y: Math.floor(this.tileMap.map.length / 2) };
+        const path = this.aStar.findPath(start, end);
+
+        for (let node of path) {
+            this.tileMap.map[node.y][node.x] = 'water';
+        }
     }
 }
