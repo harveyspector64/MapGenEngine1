7bf4f0d2a988a49163dd8e3d13527ec34f6bffcb
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 21:07:39 -0700

    Update RoadGenerator.js

--- scripts/RoadGenerator.js
+++ scripts/RoadGenerator.js
@@ -1,29 +1,35 @@
 // File: scripts/RoadGenerator.js
 
-class RoadGenerator {
-    constructor(tileMap, aStar) {
-        this.tileMap = tileMap;
-        this.aStar = aStar;
-    }
-
-    generateRoads() {
-        const start = { x: 0, y: 0 };
-        const end = { x: this.tileMap.map[0].length - 1, y: this.tileMap.map.length - 1 };
-        const path = this.aStar.findPath(start, end);
-
-        for (let node of path) {
-            this.tileMap.map[node.y][node.x] = 'road';
+(function(global) {
+    class RoadGenerator {
+        constructor(tileMap, aStar) {
+            this.tileMap = tileMap;
+            this.aStar = aStar;
         }
 
-        // Allow roads to cross water
-        this.allowRoadCrossingWater(path);
-    }
+        generateRoads() {
+            const start = { x: 0, y: 0 };
+            const end = { x: this.tileMap.map[0].length - 1, y: this.tileMap.map.length - 1 };
+            const path = this.aStar.findPath(start, end);
 
-    allowRoadCrossingWater(path) {
-        for (let node of path) {
-            if (this.tileMap.map[node.y][node.x] === 'water') {
+            for (let node of path) {
                 this.tileMap.map[node.y][node.x] = 'road';
+            }
+
+            // Allow roads to cross water
+            this.allowRoadCrossingWater(path);
+        }
+
+        allowRoadCrossingWater(path) {
+            for (let node of path) {
+                if (this.tileMap.map[node.y][node.x] === 'water') {
+                    this.tileMap.map[node.y][node.x] = 'road';
+                }
             }
         }
     }
-}
+
+    // Expose RoadGenerator to the global object
+    global.RoadGenerator = RoadGenerator;
+
+})(window);
