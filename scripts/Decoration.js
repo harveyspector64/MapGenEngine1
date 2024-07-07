ac5cb97d0a3dd47c29d418999f065cd03bfbe13a
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 19:07:43 -0700

    Create Decoration.js

---
+++ scripts/Decoration.js
@@ -0,0 +1,9 @@
+class Decoration {
+    constructor(tileMap) {
+        this.tileMap = tileMap;
+    }
+
+    placeDecorations() {
+        // Place trees, bushes, hills, barns, and silos based on rules
+    }
+}
