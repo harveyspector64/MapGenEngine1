bf4c178fda0a61dcb065b33da5463ee298d249e5
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 19:31:19 -0700

    Update Decoration.js

--- scripts/Decoration.js
+++ scripts/Decoration.js
@@ -6,14 +6,49 @@ class Decoration {
     }
 
     placeDecorations() {
-        for (let y = 0; y < this.tileMap.map.length; y++) {
-            for (let x = 0; x < this.tileMap.map[y].length; x++) {
-                if (this.tileMap.map[y][x] === 'grass' && Math.random() < 0.1) {
+        const clusters = this.createTreeClusters();
+
+        for (let cluster of clusters) {
+            for (let { x, y } of cluster) {
+                if (this.isWithinBounds(x, y) && this.tileMap.map[y][x] === 'grass') {
                     this.tileMap.map[y][x] = 'tree';
-                } else if (this.tileMap.map[y][x] === 'grass' && Math.random() < 0.05) {
-                    this.tileMap.map[y][x] = 'bush';
                 }
             }
         }
+
+        // Place single trees
+        for (let y = 0; y < this.tileMap.map.length; y++) {
+            for (let x = 0; x < this.tileMap.map[y].length; x++) {
+                if (this.tileMap.map[y][x] === 'grass' && Math.random() < 0.05) {
+                    this.tileMap.map[y][x] = 'tree';
+                }
+            }
+        }
+    }
+
+    createTreeClusters() {
+        const clusters = [];
+        const clusterCount = 10; // Number of clusters
+        const clusterSize = 3; // Size of each cluster
+
+        for (let i = 0; i < clusterCount; i++) {
+            const startX = Math.floor(Math.random() * this.tileMap.map[0].length);
+            const startY = Math.floor(Math.random() * this.tileMap.map.length);
+            const cluster = [];
+
+            for (let x = startX; x < startX + clusterSize; x++) {
+                for (let y = startY; y < startY + clusterSize; y++) {
+                    cluster.push({ x, y });
+                }
+            }
+
+            clusters.push(cluster);
+        }
+
+        return clusters;
+    }
+
+    isWithinBounds(x, y) {
+        return x >= 0 && x < this.tileMap.map[0].length && y >= 0 && y < this.tileMap.map.length;
     }
 }
