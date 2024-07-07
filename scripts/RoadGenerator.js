1b2f7cf6cedf7b059ac56c3a28492bfb16ee854e
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-06 19:07:09 -0700

    Create RoadGenerator.js

---
+++ scripts/RoadGenerator.js
@@ -0,0 +1,10 @@
+class RoadGenerator {
+    constructor(tileMap, aStar) {
+        this.tileMap = tileMap;
+        this.aStar = aStar;
+    }
+
+    generateRoads() {
+        // Use A* to generate roads with curvature and realistic logic
+    }
+}
